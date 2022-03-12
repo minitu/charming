b74882702bea082122a53a230966645144a57627
Jaemin Choi <jchoi157@illinois.edu>
2022-03-12 17:45:59 -0500

    Fix ARCH

--- examples/examples.mk
+++ examples/examples.mk
@@ -3,5 +3,5 @@ NVCC_CU_OPTS = --std=c++11 -dc $(ARCH) -I$(NVSHMEM_PREFIX)/include -I$(CHARMING_
 ifeq ($(CHARMING_USE_MPI), 1)
 NVCC_LINK_OPTS = -ccbin=mpicxx $(ARCH) -L$(NVSHMEM_PREFIX)/lib -lnvshmem -L$(MPI_ROOT)/lib -lmpi_ibm -lcuda -lcudart -L$(CHARMING_PREFIX)/lib -lcharming
 else
-NVCC_LINK_OPTS = -arch=sm_70 -L$(NVSHMEM_PREFIX)/lib -lnvshmem -lcuda -lcudart -L$(CHARMING_PREFIX)/lib -lcharming
+NVCC_LINK_OPTS = $(ARCH) -L$(NVSHMEM_PREFIX)/lib -lnvshmem -lcuda -lcudart -L$(CHARMING_PREFIX)/lib -lcharming
 endif
