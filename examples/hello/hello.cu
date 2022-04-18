8dcc11d182b61bd2609166c10f01be2580bc2cec
Jaemin Choi <jchoi157@illinois.edu>
2022-04-18 00:08:30 -0400

    Update hello example

--- examples/hello/hello.cu
+++ examples/hello/hello.cu
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "hello.h"
 
-__device__ charm::chare_proxy<Hello>* hello_proxy;
+__shared__ charm::chare_proxy<Hello>* hello_proxy;
 
 __device__ void charm::register_chares() {
   // Register Hello chare and its entry methods
@@ -11,25 +11,38 @@ __device__ void charm::register_chares() {
 
 __device__ void Hello::greet(void* arg) {
   int recv_int = ((int*)arg)[0];
-  printf("Hello I'm %d of %d! Received %d\n", i, n, recv_int);
+  __shared__ int send_int;
+  int i = charm::chare::i;
+  int n = charm::chare::n;
+  if (threadIdx.x == 0) {
+    printf("Hello I'm %d of %d! Received %d\n", i, n, recv_int);
+  }
 
   if (i == n-1) {
-    charm::end();
+    if (threadIdx.x == 0) {
+      charm::end();
+    }
   } else {
-    int send_int[1] = {recv_int + 1};
-    hello_proxy->invoke(i + 1, 0, send_int, sizeof(int));
+    if (threadIdx.x == 0) {
+      send_int = recv_int + 1;
+    }
+    __syncthreads();
+    hello_proxy->invoke(i + 1, 0, &send_int, sizeof(int));
   }
 }
 
 // Main
 __device__ void charm::main(int argc, char** argv, size_t* argvs) {
+  __shared__ int send_int;
   // Create and populate object that will become the basis of chares
   Hello my_obj;
 
   // Create chares using the data in my object
-  hello_proxy->create(my_obj, 20);
+  hello_proxy->create(my_obj, charm::n_pes() * 2);
 
   // Invoke entry method
-  int send_int[1] = {0};
-  hello_proxy->invoke(0, 0, send_int, sizeof(int));
+  if (threadIdx.x == 0) {
+    send_int = 0;
+  }
+  hello_proxy->invoke(0, 0, &send_int, sizeof(int));
 }
