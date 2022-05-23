fc36d48e5be603add6ac423d09ad7c11286cc40d
Jaemin Choi <jchoi157@illinois.edu>
2022-05-23 17:49:27 -0400

    Update hello world example

--- examples/hello/hello.cu
+++ examples/hello/hello.cu
@@ -3,28 +3,35 @@
 
 __shared__ charm::chare_proxy<Hello>* hello_proxy;
 
-__device__ void charm::create_chares(int argc, char** argv, size_t* argvs) {
-  // Register Hello chare and its entry methods
-  hello_proxy = new charm::chare_proxy<Hello>();
-  hello_proxy->add_entry_method<&entry_greet>();
-  hello_proxy->create(charm::n_pes() * 2);
-}
-
-__device__ void charm::main(int argc, char** argv, size_t* argvs) {
-  __shared__ int send_int;
-
+__device__ void charm::main(int argc, char** argv, size_t* argvs, int pe) {
+  // Execute on all elements
   if (threadIdx.x == 0) {
-    send_int = 0;
+    // Register Hello chare and its entry methods
+    hello_proxy = new charm::chare_proxy<Hello>();
+    hello_proxy->add_entry_method<&entry_greet>();
+    hello_proxy->create(charm::n_pes() * 2);
   }
   __syncthreads();
 
-  // Send integer to first chare
-  hello_proxy->invoke(0, 0, &send_int, sizeof(int));
+  barrier();
+
+  // Execute only on PE 0
+  if (pe == 0) {
+    int* send_int_p = nullptr;
+    if (threadIdx.x == 0) {
+      send_int_p = new int;
+      *send_int_p = 0;
+    }
+    __syncthreads();
+
+    // Send integer to first chare
+    hello_proxy->invoke(0, 0, send_int_p, sizeof(int));
+  }
 }
 
 __device__ void Hello::greet(void* arg) {
-  int recv_int = ((int*)arg)[0];
-  __shared__ int send_int;
+  int recv_int = *(int*)arg;
+  int* send_int_p;
   int i = charm::chare::i;
   int n = charm::chare::n;
   if (threadIdx.x == 0) {
@@ -35,10 +42,11 @@ __device__ void Hello::greet(void* arg) {
     charm::end();
   } else {
     if (threadIdx.x == 0) {
-      send_int = recv_int + 1;
+      send_int_p = new int;
+      *send_int_p = recv_int + 1;
     }
     __syncthreads();
 
-    hello_proxy->invoke(i + 1, 0, &send_int, sizeof(int));
+    hello_proxy->invoke(i + 1, 0, send_int_p, sizeof(int));
   }
 }
